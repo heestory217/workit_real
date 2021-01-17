447a34eb4a36e55f85a20adba9dbfa5d4001e0e4
ebn427 <67459851+ebn427@users.noreply.github.com>
2021-01-18 01:47:41 +0900

    [은별] update UsersController.java (내용추가)

--- src/main/java/com/it/workit/users/controller/UsersController.java
+++ src/main/java/com/it/workit/users/controller/UsersController.java
@@ -3,7 +3,10 @@ package com.it.workit.users.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/users")
@@ -16,5 +19,20 @@ public class UsersController {
 		logger.info("개인회원 회원가입 화면 보여주기");
 		
 		return "users/register";
+	}
+	
+	@RequestMapping("/checkId.do")
+	public String checkId(@RequestParam String userid, Model model) {
+		logger.info("아이디 중복 확인, userid = {}",userid);
+		
+		int result=0;
+		
+		logger.info("아이디 중복 확인결과, result = {}",result);
+		
+		model.addAttribute("result", result);
+		//model.addAttribute("EXIST_ID", MemberService.EXIST_ID);
+		//model.addAttribute("NON_EXIST_ID", MemberService.NON_EXIST_ID);
+
+		return "users/checkId";
 	}
 }
