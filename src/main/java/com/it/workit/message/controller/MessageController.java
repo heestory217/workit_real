801c1e389a6ea5e93984858019543a3f6d1a04e5
heestory217 <heestory217@gmail.com>
2021-01-18 23:48:33 +0900

    [희정] 쪽지 상세보기 view 완성

--- src/main/java/com/it/workit/message/controller/MessageController.java
+++ src/main/java/com/it/workit/message/controller/MessageController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -43,4 +44,8 @@ public class MessageController {
 	
 	@RequestMapping("/messageBoxSend.do")
 	public void messageBoxSend() {};
+	
+	@RequestMapping(value="/messageDetail.do", method = RequestMethod.GET)
+	public void messageDetail() {};
+	
 }
