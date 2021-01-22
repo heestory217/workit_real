ad9da31923901c22a8a2c5e7ef78cea5ccfe6bc9
ebn427 <67459851+ebn427@users.noreply.github.com>
2021-01-23 00:00:09 +0900

    [은별] 개인마이페이지 - 회원정보 수정 전 비밀번호 확인 기능

--- src/main/java/com/it/workit/indivMypage/controller/IndivMypageController.java
+++ src/main/java/com/it/workit/indivMypage/controller/IndivMypageController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.it.workit.users.model.UsersService;
 import com.it.workit.users.model.UsersVO;
@@ -21,19 +23,23 @@ public class IndivMypageController {
 	
 	@Autowired private UsersService userService;
 	
+	//비밀번호 입력 -> 수정화면을 거치기 위해서 단순 get방식은 indivCheckPwd로 리턴된다.
 	@RequestMapping(value = "/indivMypageEdit.do", method = RequestMethod.GET)
-	public String usersEdit_get(Model model) {
+	public String usersEdit_get() {
 		//세션 userid 가져오기
 		//String userid=(String) session.getAttribute("userid");
-		String userid="kang";	//임시
-		logger.info("개인 마이페이지 - 수정 화면 보여주기 / 파라미터 userid = {}",userid);
+		//String userid="kang";	//임시
+		//logger.info("개인 마이페이지 - 수정 화면 보여주기 / 파라미터 userid = {}",userid);
 		
-		UsersVO vo=userService.selectByUserId(userid);
-		logger.info("마이페이지 - 회원 수정 페이지 / 조회 결과 vo={}", vo);
+		//UsersVO vo=userService.selectByUserId(userid);
+		//logger.info("마이페이지 - 회원 수정 페이지 / 조회 결과 vo={}", vo);
 		
-		model.addAttribute("vo", vo);
+		//model.addAttribute("vo", vo);
 		
-		return "indivMypage/indivMypageEdit";
+		//return "indivMypage/indivMypageEdit";
+		
+		logger.info("indivCheckPwd없이는 edit 창 진입불가, indivCheckPwd 리턴");
+		return "indivMypage/indivCheckPwd";
 	}
 	
 	@RequestMapping(value = "/indivMypageEdit.do", method = RequestMethod.POST)
@@ -72,6 +78,33 @@ public class IndivMypageController {
 		logger.info("개인 마이페이지 - 비밀번호 체크 화면 보여주기");
 		
 		return "indivMypage/indivCheckPwd";
+	}
+	
+	@RequestMapping(value = "/indivCheckPwd.do", method = RequestMethod.POST)
+	public String checkPwd_post(@RequestParam(defaultValue = "0") String pwd, Model model) {
+		
+		logger.info("개인 마이페이지 - 비밀번호 파라미터 확인 pwd={}", pwd);
+		
+		//세션 userid 가져오기
+		//String userid=(String) session.getAttribute("userid");
+		String userid="kang";
+		
+		UsersVO vo=userService.selectByUserId(userid);
+		logger.info("개인 마이페이지 - 비밀번호 체크 / DB pwd 조회 결과 pwd={}", vo.getUserPassword());
+		
+		if(vo.getUserPassword().equals(pwd)) {
+			
+			model.addAttribute("vo", vo);
+			
+			return "indivMypage/indivMypageEdit";
+		}else {
+			String msg="비밀번호가 틀립니다. 다시 시도해주세요.", url="/indivMypage/indivCheckPwd.do";
+			
+			model.addAttribute("msg",msg);
+			model.addAttribute("url",url);
+			
+			return "common/message";
+		}
 	}
 	
 }
