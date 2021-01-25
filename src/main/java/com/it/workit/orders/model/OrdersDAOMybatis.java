ceaa8cd0449ca37c6cf5203e9a3d24ddcee57875
ebn427 <67459851+ebn427@users.noreply.github.com>
2021-01-25 23:40:23 +0900

    [은별] 개인 마이페이지 - 결제 내역 / orders 관련

--- src/main/java/com/it/workit/orders/model/OrdersDAOMybatis.java
+++ src/main/java/com/it/workit/orders/model/OrdersDAOMybatis.java
@@ -1,13 +1,21 @@
 package com.it.workit.orders.model;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OrdersDAOMybatis implements OrdersDAO{
 
-	@Autowired SqlSession sqlSession;
+	@Autowired SqlSessionTemplate sqlSession;
 	
-	String namespace="config.mybatis.mapper.oracle.orders.";
+	private String namespace="config.mybatis.mapper.oracle.orders.";
+
+	@Override
+	public List<OrdersVO> selectIndivPaymentByUserno(int userNo) {
+		return sqlSession.selectList(namespace+"selectIndivPaymentByUserno",userNo);
+	}
 }
--- src/main/java/com/it/workit/orders/model/OrdersServiceImpl.java
+++ src/main/java/com/it/workit/orders/model/OrdersServiceImpl.java
@@ -1,5 +1,7 @@
 package com.it.workit.orders.model;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +9,9 @@ import org.springframework.stereotype.Service;
 public class OrdersServiceImpl implements OrdersService{
 
 	@Autowired OrdersDAO ordersDao;
+
+	@Override
+	public List<OrdersVO> selectIndivPaymentByUserno(int userNo) {
+		return ordersDao.selectIndivPaymentByUserno(userNo);
+	}
 }
