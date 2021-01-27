924aa93e70bcd05198cea3d33278b92771da27c2
ebn427 <67459851+ebn427@users.noreply.github.com>
2021-01-27 17:53:26 +0900

    [은별] 개인 마이페이지 - 결제내역 수정 및 페이징 처리

--- src/main/java/com/it/workit/orders/model/OrdersServiceImpl.java
+++ src/main/java/com/it/workit/orders/model/OrdersServiceImpl.java
@@ -5,13 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.it.workit.indivMypage.model.IndivpagingVO;
+
 @Service
 public class OrdersServiceImpl implements OrdersService{
 
 	@Autowired OrdersDAO ordersDao;
 
 	@Override
-	public List<OrdersVO> selectIndivPaymentByUserno(int userNo) {
-		return ordersDao.selectIndivPaymentByUserno(userNo);
+	public List<OrdersVO> selectIndivPaymentByUserno(IndivpagingVO vo) {
+		return ordersDao.selectIndivPaymentByUserno(vo);
+	}
+
+	@Override
+	public int ordersGetTotalRecord(IndivpagingVO vo) {
+		return ordersDao.ordersGetTotalRecord(vo);
 	}
 }
