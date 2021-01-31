a6ebc2f395634fec2a3ccc83b7f44b94556fa867
ebn427 <67459851+ebn427@users.noreply.github.com>
2021-01-31 17:24:32 +0900

    [은별] 개인 검색 모델 수정

--- src/main/java/com/it/workit/indivsearch/model/IndivSearchDAOMybatis.java
+++ src/main/java/com/it/workit/indivsearch/model/IndivSearchDAOMybatis.java
@@ -6,6 +6,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.it.workit.language.model.LanguageVO;
+
 @Repository
 public class IndivSearchDAOMybatis implements IndivSearchDAO{
 	@Autowired
@@ -16,5 +18,15 @@ public class IndivSearchDAOMybatis implements IndivSearchDAO{
 	@Override
 	public List<IndivKeywordSearchVO> selectIndivKeyword(String keyword) {
 		return sqlSession.selectList(namespace+"selectIndivKeyword",keyword);
+	}
+
+	@Override
+	public List<IndivKeywordSearchVO> selectIndivLanguage(int languageNo) {
+		return sqlSession.selectList(namespace+"selectIndivLanguage",languageNo);
+	}
+
+	@Override
+	public List<LanguageVO> selectLanguage() {
+		return sqlSession.selectList(namespace+"selectLanguage");
 	}
 }
--- src/main/java/com/it/workit/indivsearch/model/IndivSearchService.java
+++ src/main/java/com/it/workit/indivsearch/model/IndivSearchService.java
@@ -2,6 +2,10 @@ package com.it.workit.indivsearch.model;
 
 import java.util.List;
 
+import com.it.workit.language.model.LanguageVO;
+
 public interface IndivSearchService {
 	public List<IndivKeywordSearchVO> selectIndivKeyword(String keyword);
+	public List<IndivKeywordSearchVO> selectIndivLanguage(int languageNo);
+	public List<LanguageVO> selectLanguage();
 }
