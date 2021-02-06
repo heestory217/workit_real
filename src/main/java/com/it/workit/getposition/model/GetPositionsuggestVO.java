d86c97fa265d71a0dd89948748dd09bf334398ee
heestory217 <heestory217@gmail.com>
2021-02-06 16:56:05 +0900

    [희정] 포지션제안받는 테이블 vo 수정 - 중요여부컬럼1 삭제

--- src/main/java/com/it/workit/getposition/model/GetPositionsuggestVO.java
+++ src/main/java/com/it/workit/getposition/model/GetPositionsuggestVO.java
@@ -5,7 +5,6 @@ public class GetPositionsuggestVO {
 	private int userNo;	//받는 회원번호
 	private int getPositionsuggestReadFlag;	//읽기여부
 	private int getPositionsuggestDelFlag;	//삭제여부
-	private int getPositionsuggestImpFlag;	//중요제안여부
 	private int positionsuggestNo;	//포지션제안번호
 
 	public int getGetPositionNo() {
@@ -32,12 +31,6 @@ public class GetPositionsuggestVO {
 	public void setGetPositionsuggestDelFlag(int getPositionsuggestDelFlag) {
 		this.getPositionsuggestDelFlag = getPositionsuggestDelFlag;
 	}
-	public int getGetPositionsuggestImpFlag() {
-		return getPositionsuggestImpFlag;
-	}
-	public void setGetPositionsuggestImpFlag(int getPositionsuggestImpFlag) {
-		this.getPositionsuggestImpFlag = getPositionsuggestImpFlag;
-	}
 	public int getPositionsuggestNo() {
 		return positionsuggestNo;
 	}
@@ -47,9 +40,9 @@ public class GetPositionsuggestVO {
 	
 	@Override
 	public String toString() {
-		return "GetPositionVO [getPositionNo=" + getPositionNo + ", userNo=" + userNo + ", getPositionsuggestReadFlag="
-				+ getPositionsuggestReadFlag + ", getPositionsuggestDelFlag=" + getPositionsuggestDelFlag
-				+ ", getPositionsuggestImpFlag=" + getPositionsuggestImpFlag + ", positionsuggestNo="
-				+ positionsuggestNo + "]";
+		return "GetPositionsuggestVO [getPositionNo=" + getPositionNo + ", userNo=" + userNo
+				+ ", getPositionsuggestReadFlag=" + getPositionsuggestReadFlag + ", getPositionsuggestDelFlag="
+				+ getPositionsuggestDelFlag + ", positionsuggestNo=" + positionsuggestNo + "]";
 	}
+
 }
