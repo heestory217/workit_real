374729ea0baedc2fed27c9b25e3c665e66d3f40c
heestory217 <heestory217@gmail.com>
2021-02-14 00:55:42 +0900

    [희정] 관리자페이지 jsp

---
+++ src/main/java/com/it/workit/admin/website/controller/AdminWebsiteManageController.java
@@ -0,0 +1,29 @@
+package com.it.workit.admin.website.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/siteManage")
+public class AdminWebsiteManageController {
+	private static final Logger logger = LoggerFactory.getLogger(AdminWebsiteManageController.class);
+	
+	@RequestMapping("/siteIntro.do")
+	public void siteIntro() {
+		logger.info("사이트소개 페이지 보여주기");
+	}
+			
+	@RequestMapping("/termsOfService.do")
+	public void termsOfService() {
+		logger.info("이용약관 페이지 보여주기");
+		
+	}
+	
+	@RequestMapping("/privacyPolicy.do")
+	public void privacyPolicy() {
+		logger.info("개인정보처리방침 페이지 보여주기");
+		
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/admin/siteManage/siteIntro.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="../inc/top.jsp"%>
+사이트 소개 페이지
+<%@ include file="../inc/bottom.jsp"%>
