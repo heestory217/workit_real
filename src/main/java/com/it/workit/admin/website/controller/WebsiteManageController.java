8b5449be504ca8386136bdd73ba2b525e91ff461
heestory217 <heestory217@gmail.com>
2021-02-15 00:24:39 +0900

    [희정] 웹사이트 관리 구현 완료

--- src/main/java/com/it/workit/admin/website/controller/WebsiteManageController.java
+++ src/main/java/com/it/workit/admin/website/controller/WebsiteManageController.java
@@ -1,24 +1,33 @@
 package com.it.workit.admin.website.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.it.workit.admin.website.model.WebsiteManageService;
 
 @Controller
 @RequestMapping("/website")
 public class WebsiteManageController {
 	private static final Logger logger = LoggerFactory.getLogger(WebsiteManageController.class);
+	@Autowired private WebsiteManageService websiteService;
 	
 	@RequestMapping("/termsOfService.do")
-	public void termsOfService() {
+	public void termsOfService(Model model) {
 		logger.info("이용약관 페이지 보여주기");
-		
+		Map<String, Object> map = websiteService.selectWebsiteManage("T");
+		model.addAttribute("map", map);
 	}
 	
 	@RequestMapping("/privacyPolicy.do")
-	public void privacyPolicy() {
+	public void privacyPolicy(Model model) {
 		logger.info("개인정보처리방침 페이지 보여주기");
-		
+		Map<String, Object> map = websiteService.selectWebsiteManage("P");
+		model.addAttribute("map", map);
 	}
 }
--- src/main/webapp/WEB-INF/views/website/privacyPolicy.jsp
+++ src/main/webapp/WEB-INF/views/website/privacyPolicy.jsp
@@ -1,5 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ include file="../inc/top.jsp"%>
-개인정보처리방침 페이지
+
+<style>
+.col-lg-12 h4 {
+	font-weight: bold;
+	text-align: center;
+	margin-top: 20px;
+}
+
+#policy p {
+	font-size: smaller;
+}
+</style>
+
+<div class="breacrumb-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-text product-more">
+                    <a href="<c:url value='/index.do'/>"><i class="fa fa-home"></i> Home</a>
+                    <a href="#">Website</a>
+                    <span>Privacy Policy</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+        	<h4>개인정보처리방침</h4>
+        	<hr>
+        	<div id="policy" style="padding: 20px 20px;">
+        		${map['websitemanageAbout']}
+        	</div>
+		</div>
+	</div>
+</div>
+
+
+
 <%@ include file="../inc/bottom.jsp"%>
